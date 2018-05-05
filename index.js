c477d5e169dc3cc3099fb42e580462945d25bbff
Dushyant Singh Shekhawat <dushyant225@gmail.com>
2018-05-05 17:19:01 +0530

    Include redux store.

--- index.js
+++ index.js
@@ -1,4 +1,16 @@
+import React from 'react';
 import { AppRegistry } from 'react-native';
+import { createStore } from 'redux';
+import { Provider } from 'react-redux';
+import { reducer } from './app/data/redux/reducer';
 import App from './app/App';
 
-AppRegistry.registerComponent('ToDoList', () => App);
+const store = createStore(reducer);
+
+const AppWithStore = () => (
+  <Provider store={store}>
+    <App />
+  </Provider>
+);
+
+AppRegistry.registerComponent('ToDoList', () => AppWithStore);
